1f93afa45708db3bd3ae8a153959deb39c2bde7e
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2019-11-28 18:07:56 +0530

    added testcases for performCmd function
    What changes you have made?
    ....Now date validation also can be done
    
    Why this change is necessary?
    ....to query transactions based on date it has to be validated and
    checked it in transaction records.
    
    What is the impact of the change?
    .....transactions  can be filtered based on date also

--- test/testPerformOperation.js
+++ test/testPerformOperation.js
@@ -1,228 +1,226 @@
-const getPair = require("../src/performOperation").getPair;
-const performOperation = require("../src/performOperation").performOperation;
-const getTransDetails = require("../src/existingEmpsJuiceTRansDetails")
-  .getTransDetails;
 const assert = require("assert");
-const fs = require("fs");
+const performCmd = require("../src/performOperation").performCmd;
+const getPair = require("../src/performOperation").getPair;
 
-describe("get paired argument", function() {
+describe("test for getPair function", function() {
+  it("should pair the argument", function() {
+    assert.deepStrictEqual(getPair(["--empId", "1111"]), [["--empId", "1111"]]);
+  });
+
   it("should pair the arguments", function() {
-    const expected = [
-      ["--beverage", "orange"],
-      ["--empId", "11111"],
+    assert.deepStrictEqual(getPair(["--empId", "1111", "--qty", "1"]), [
+      ["--empId", "1111"],
       ["--qty", "1"]
-    ];
-    assert.deepStrictEqual(
-      getPair(["--beverage", "orange", "--empId", "11111", "--qty", "1"]),
-      expected
-    );
-  });
-
-  it("should pair the arguments when the arguments length is odd", function() {
-    const expected = [
-      ["--beverage", "orange"],
-      ["--empId", "11111"],
-      ["--qty", undefined]
-    ];
-    assert.deepStrictEqual(
-      getPair(["--beverage", "orange", "--empId", "11111", "--qty"]),
-      expected
-    );
+    ]);
   });
 });
 
-describe("perform query operation", function() {
-  it("should validate options and decide to perform query operation", function() {
-    const actualArgs = ["--query", "--empId", "111"];
-    const actualObject = {
-      "111": [{ beverage: "Apple", qty: 1, date: "21-11-2000" }]
+describe("test performcommand function for save command", function() {
+  it("should validate and perform command based on arguments", function() {
+    let fakeDate = new Date();
+    expectedDate = fakeDate.toJSON();
+    const writeIntoFile = function(path, newTransaction) {
+      assert.deepEqual(path, "./juiceTransacDetails");
+      assert.deepStrictEqual(newTransaction, [
+        { empId: "1120", beverage: "Grapes", qty: "1", date: fakeDate }
+      ]);
     };
-    const date = new Date();
-    const expected =
-      "Employee ID,Beverage,Quantity,Date\n111,Apple,1,21-11-2000\nTotal: 1 Juices";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
-      expected
-    );
-  });
-  it("should not query when the employee id doesn't exist in the record", function() {
-    const actualArgs = ["--query", "--empId", "111"];
-    const actualObject = {
-      "111": [{ beverage: "Apple", qty: 1, date: "21-11-2000" }],
-      "112": [
-        { beverage: "Apple", qty: 1, date: "21-11-2000" },
-        { beverage: "Apple", qty: 1, date: "21-11-2000" }
-      ]
+    const dateGenerator = function() {
+      return fakeDate;
     };
-    const date = new Date();
-    const expected =
-      "Employee ID,Beverage,Quantity,Date\n111,Apple,1,21-11-2000\nTotal: 1 Juices";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
-      expected
-    );
-  });
 
-  it("should not query when the employee id doesn't exist in the record", function() {
-    const actualArgs = ["--query", "--empId", "113"];
-    const actualObject = {};
-    const date = new Date();
-    const expected = "Enter valid option";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
-      expected
-    );
-  });
-
-  it("should not query when the multiple employee id is given", function() {
-    const actualArgs = ["--query", "--empId", "113", "--empId", "112"];
-    const actualObject = {};
-    const date = new Date();
-    const expected = "Enter valid option";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
-      expected
-    );
-  });
-
-  it("should not query option when the required feild is not given ", function() {
-    const actualArgs = ["--query", "--empId", "113", "--beverage", "Banana"];
-    const actualObject = {};
-    const date = new Date();
-    const expected = "Enter valid option";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
-      expected
-    );
-  });
-});
-
-describe("perform save operation", function() {
-  it("should validate options and decide to perform save operation", function() {
-    const actualArgs = [
+    const existingTrans = [];
+    const usrArgs = [
       "--save",
       "--beverage",
-      "Apple",
+      "Grapes",
       "--qty",
       "1",
       "--empId",
-      "115"
+      "1120"
     ];
-    const actualObject = {};
-    const date = "21-12-2000";
-    const expected =
-      "Transaction Record:\nEmployee ID,Beverage,Quantity,Date\n115,Apple,1,21-12-2000";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
+
+    expected =
+      "Transaction Recorded:\nEmployee ID,Beverage,Quantity,Date\n1120,Grapes,1," +
+      expectedDate;
+    assert.deepStrictEqual(
+      performCmd(
+        existingTrans,
+        usrArgs,
+        "./juiceTransacDetails",
+        dateGenerator,
+        writeIntoFile
+      ),
       expected
     );
   });
 
-  it("should validate options and decide to perform save operation", function() {
-    const actualArgs = [
+  it("should validate and perform command based on arguments", function() {
+    let fakeDate = new Date();
+    expectedDate = fakeDate.toJSON();
+    const writeIntoFile = function(path, newTransaction) {
+      assert.deepEqual(path, "./juiceTransacDetails");
+      assert.deepStrictEqual(newTransaction, [
+        { empId: "1124", beverage: "Banana", qty: "2", date: fakeDate }
+      ]);
+    };
+    const dateGenerator = function() {
+      return fakeDate;
+    };
+
+    const existingTrans = [];
+    const usrArgs = [
       "--save",
       "--beverage",
       "Banana",
       "--qty",
       "2",
       "--empId",
-      "115"
+      "1124"
     ];
-    const actualObject = {};
-    const date = "1-2-2000";
-    const expected =
-      "Transaction Record:\nEmployee ID,Beverage,Quantity,Date\n115,Banana,2,1-2-2000";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
+
+    expected =
+      "Transaction Recorded:\nEmployee ID,Beverage,Quantity,Date\n1124,Banana,2," +
+      expectedDate;
+    assert.deepStrictEqual(
+      performCmd(
+        existingTrans,
+        usrArgs,
+        "./juiceTransacDetails",
+        dateGenerator,
+        writeIntoFile
+      ),
       expected
     );
   });
 
-  it("should not validate save wrong options ", function() {
-    const actualArgs = [
-      "--sav",
-      "--beverage",
-      "Apple",
-      "--qty",
-      "1",
-      "--empId",
-      "115"
-    ];
-    const actualObject = {};
-    const date = "21-12-2000";
-    const expected = "Enter valid option";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
-      expected
+  it("should validate and return ERROR message when the arguments are not valid", function() {
+    const usrArgs = ["--save", "--bvg", "Orange"];
+    const dateGenerator = function() {};
+    const writeIntoFile = function() {};
+    assert.strictEqual(
+      performCmd(
+        [],
+        usrArgs,
+        "./juiceTransacDetails",
+        dateGenerator,
+        writeIntoFile
+      ),
+      "Enter valid option"
     );
   });
 
-  it("should not validate save wrong options ", function() {
-    const actualArgs = ["--save", "--empId", "115"];
-    const actualObject = {};
-    const date = "21-12-2000";
-    const expected = "Enter valid option";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
-      expected
+  it("should validate and return ERROR message when the arguments are not valid", function() {
+    const usrArgs = ["--save", "--beverage", "Orange"];
+    const dateGenerator = function() {};
+    const writeIntoFile = function() {};
+    assert.strictEqual(
+      performCmd(
+        [],
+        usrArgs,
+        "./juiceTransacDetails",
+        dateGenerator,
+        writeIntoFile
+      ),
+      "Enter valid option"
     );
   });
 
-  it("should not validate save options, when the options are jumbled ", function() {
-    const actualArgs = [
+  it("should validate and return ERROR message when the arguments are not valid", function() {
+    const usrArgs = [
       "--save",
-      "--empId",
-      "115",
+      "--beverage",
+      "Orange",
       "--qty",
       "1",
-      "--beverage",
-      "Apple"
-    ];
-    const actualObject = {};
-    const date = "21-12-2000";
-    const expected =
-      "Transaction Record:\nEmployee ID,Beverage,Quantity,Date\n115,Apple,1,21-12-2000";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
-      expected
-    );
-  });
-  it("should not validate save options, when the options are more", function() {
-    const actualArgs = [
-      "--save",
       "--empId",
-      "115",
-      "--qty",
-      "1",
-      "--beverage",
-      "Apple",
-      "--empId",
-      "112"
+      "-19"
     ];
-    const actualObject = {};
-    const date = "21-12-2000";
-    const expected = "Enter valid option";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
-      expected
+    const dateGenerator = function() {};
+    const writeIntoFile = function() {};
+    assert.strictEqual(
+      performCmd(
+        [],
+        usrArgs,
+        "./juiceTransacDetails",
+        dateGenerator,
+        writeIntoFile
+      ),
+      "Enter valid option"
     );
   });
 
-  it("should not save transaction when the required feild is not given ", function() {
-    const actualArgs = ["--save", "--empId", "113", "--beverage", "Banana"];
-    const actualObject = {};
-    const date = new Date();
-    const expected = "Enter valid option";
-    assert.deepEqual(
-      performOperation(actualObject, actualArgs, "./testForSave.json", date),
-      expected
+  it("should validate and return ERROR message when the arguments are not valid", function() {
+    const usrArgs = [
+      "--save",
+      "--beverage",
+      "Tomato",
+      "--qty",
+      "1",
+      "--empId",
+      "1119"
+    ];
+    const dateGenerator = function() {};
+    const writeIntoFile = function() {};
+    assert.strictEqual(
+      performCmd(
+        [],
+        usrArgs,
+        "./juiceTransacDetails",
+        dateGenerator,
+        writeIntoFile
+      ),
+      "Enter valid option"
     );
   });
 });
 
-describe("gettingObject", function() {
-  it("should return object by parsing it to Json", function() {
-    const expected = JSON.parse(fs.readFileSync("./testForSave.json"));
-    assert.deepStrictEqual(getTransDetails("./testForSave.json"), expected);
+describe("test performCommand function for query command", function() {
+  it("should validate arguments and filter transactions based on empId", function() {
+    const dateGenerator = function() {};
+    const writeIntoFile = function() {};
+    const existingTrans = [
+      { empId: "1111", beverage: "Orange", qty: "1", date: "2019-11-01" }
+    ];
+    const usrArgs = ["--query", "--empId", "1111"];
+    const expected =
+      "Employee ID, Beverage, Quantity, Date\n1111,Orange,1,2019-11-01\nTotal: 1 Juice";
+    assert.strictEqual(
+      performCmd(existingTrans, usrArgs, "./juiceTransaction", writeIntoFile),
+      expected
+    );
+  });
+
+  it("should validate arguments and filter transactions based on date", function() {
+    const dateGenerator = function() {};
+    const writeIntoFile = function() {};
+    const existingTrans = [
+      { empId: "1111", beverage: "Orange", qty: "2", date: "2019-11-01" },
+      { empId: "1111", beverage: "Bangalore", qty: "1", date: "2019-10-02" }
+    ];
+    const usrArgs = ["--query", "--date", "2019-11-01"];
+    const expected =
+      "Employee ID, Beverage, Quantity, Date\n1111,Orange,2,2019-11-01\nTotal: 2 Juice";
+    assert.strictEqual(
+      performCmd(existingTrans, usrArgs, "./juiceTransaction", writeIntoFile),
+      expected
+    );
+  });
+
+  it("should validate arguments and filter transactions based on date and empId", function() {
+    const dateGenerator = function() {};
+    const writeIntoFile = function() {};
+    const existingTrans = [
+      { empId: "1111", beverage: "Orange", qty: "2", date: "2019-11-01" },
+      { empId: "1112", beverage: "Bangalore", qty: "1", date: "2019-10-01" },
+      { empId: "1111", beverage: "Banana", qty: "2", date: "2019-11-01" }
+    ];
+    const usrArgs = ["--query", "--date", "2019-11-01"];
+    const expected =
+      "Employee ID, Beverage, Quantity, Date\n1111,Orange,2,2019-11-01\n1111,Banana,2,2019-11-01\nTotal: 4 Juice";
+    assert.strictEqual(
+      performCmd(existingTrans, usrArgs, "./juiceTransaction", writeIntoFile),
+      expected
+    );
   });
 });
